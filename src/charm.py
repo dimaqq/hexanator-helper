14b97455cd273ce2b175284f1b23e04829073bba
Dima Tisnek <dimaqq@gmail.com>
2025-03-10 14:33:07 +0900

    first post!

---
+++ src/charm.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical
+"""Charmed Gubernator."""
+import json
+import logging
+
+import ops
+
+# NOTE: adjust to your local environment
+# the ip address is what process in a pod can use to access the vm
+URL="http://192.168.107.4:4318/v1/traces"
+
+
+class HexanatorHelperCharm(ops.CharmBase):
+    """Charm the service."""
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+        self.framework.observe(self.on.charm_tracing_relation_created, self._on_charm_tracing_changed)
+        self.framework.observe(self.on.send_ca_cert_relation_created, self._on_send_ca_cert_changed)
+
+    def _on_charm_tracing_changed(self, event: ops.RelationCreatedEvent):
+        relation = event.relation
+        relation.data[self.unit]["receivers"] = json.dumps([
+            {
+                "protocol": {"name": "otlp_http", "type": "http"},
+                "url": URL,
+            }
+        ])
+        logging.info("Set tracing destination data on relation %s", relation.id)
+
+    def _on_send_ca_cert_changed(self, event: ops.RelationCreatedEvent):
+        relation = event.relation
+        relation.data[self.unit]["certificates"] = json.dumps(["FIXME--CERT", "FIXME--CERT"])
+        logging.info("Set CA certificates on relation %s", relation.id)
+
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(HexanatorHelperCharm)  # type: ignore
